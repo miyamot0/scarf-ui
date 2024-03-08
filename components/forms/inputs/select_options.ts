61f23295c5d999487495d8b072a29ff737324d60
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-03-08 14:30:19 -0600

    add in select options, make nice and easily replicable

---
+++ components/forms/inputs/select_options.ts
@@ -0,0 +1,29 @@
+export const OptionsYesNo = ['', 'Yes', 'No'] as const
+
+export const OptionsYesNoNA = ['', 'Yes', 'No', 'N/A'] as const
+
+export const PrimarySecondaryUnknown = [
+    '',
+    'Primary',
+    'Secondary',
+    'Unknown',
+] as const
+
+export const StrongWeakCounterTherapeutic = [
+    '',
+    'Strong',
+    'Weak',
+    'Inconsistent',
+    'Null',
+    'Countertherapeutic',
+] as const
+
+export const StrongWeakCounterTherapeuticNA = [
+    '',
+    'Strong',
+    'Weak',
+    'Inconsistent',
+    'Null',
+    'Countertherapeutic',
+    'N/A',
+] as const
