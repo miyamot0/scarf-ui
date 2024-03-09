b82a9394d5869981229a54cb65b2aba720be1896
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-03-08 23:11:26 -0600

    add in latest

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import MaxWidthWrapper from '@/components/ui/max_width_wrapper'
 
 export default function Home() {
     return (
-        <MaxWidthWrapper>
+        <MaxWidthWrapper className="flex flex-col gap-y-4">
+            <h1 className="text-2xl font-bold text-center my-4">
+                Single Case Analysis and Review Framework
+            </h1>
             <MainPage />
+            <span className="text-center">Designed by Shawn Gilroy</span>
         </MaxWidthWrapper>
     )
 }
