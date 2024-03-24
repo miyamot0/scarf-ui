bdcbc1c5e38e0f561c0e85eb68b54b43642d5ee2
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-03-24 16:02:17 -0500

    Add in autosave fx

--- atoms/reducers/reducer.ts
+++ atoms/reducers/reducer.ts
@@ -11,10 +11,16 @@ import { StudyObject } from '@/questions/types/QuestionTypes'
 
 const KEY_LOCAL_STORAGE = 'scarf-web-ui'
 
+const SaveToLocalStorage = (state: GlobalStateType) => {
+    localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(state))
+}
+
 export const database_reducer = (
     state: GlobalStateType,
     action: DatabaseAction
 ) => {
+    let new_state
+
     switch (action.type) {
         case 'load_local':
             const value = localStorage.getItem(KEY_LOCAL_STORAGE)
@@ -31,7 +37,7 @@ export const database_reducer = (
             return action.payload.saved_state
 
         case 'save_local':
-            localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(state))
+            SaveToLocalStorage(state)
 
             return state
 
@@ -50,12 +56,16 @@ export const database_reducer = (
                 PublicationType: 'Unclassified',
             }
 
-            return {
+            new_state = {
                 ...state,
                 Studies: [...state.Studies, new_study],
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'bulk_import_studies':
-            return {
+            new_state = {
                 ...state,
                 DialogState: {
                     dialog_type: undefined,
@@ -63,25 +73,41 @@ export const database_reducer = (
                 },
                 Studies: [...state.Studies, ...action.payload.studies],
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'remove':
-            return {
+            new_state = {
                 ...state,
                 Studies: state.Studies.filter(
                     (item) => !action.payload.study_ids.includes(item.StudyID)
                 ),
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'update_display_state':
-            return {
+            new_state = {
                 ...state,
                 DisplayState: action.payload.display_state,
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'update_dialog_state':
-            return {
+            new_state = {
                 ...state,
                 DialogState: action.payload.dialog_state,
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'update_study':
-            return {
+            new_state = {
                 ...state,
                 DialogState: {
                     dialog_type: undefined,
@@ -93,8 +119,12 @@ export const database_reducer = (
                         : item
                 ),
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'update_study_category':
-            return {
+            new_state = {
                 ...state,
                 Studies: state.Studies.map((item) =>
                     item.StudyID === action.payload.study_id
@@ -102,8 +132,12 @@ export const database_reducer = (
                         : item
                 ),
             }
+
+            if (state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         case 'update_review':
-            return {
+            new_state = {
                 ...state,
                 DialogState: {
                     dialog_type: undefined,
@@ -113,6 +147,10 @@ export const database_reducer = (
                 ReviewType: action.payload.review_type,
                 AutoSave: action.payload.auto_save,
             }
+
+            if (new_state.AutoSave) SaveToLocalStorage(new_state)
+
+            return new_state
         default:
             return state
     }
