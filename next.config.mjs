78386c0b5c27abad07e6fe2602c19a13fe79caa9
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-08-13 16:15:43 -0500

    push fixes for latest react

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-import million from 'million/compiler'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-}
-
-const millionConfig = {
-    auto: {
-        threshold: 0.05, // default: 0.1,
-        skip: ['useBadHook', /badVariable/g], // default []
-        rsc: true,
-        // if you're using RSC: auto: { rsc: true },
-    },
 }
 
 export default nextConfig
